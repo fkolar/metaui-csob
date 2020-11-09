5b8d73c9515ccb5ad8034dccbfe8aabe22cad022
fkolar <fkolar@icloud.com>
2020-11-09 15:08:01 +0100

    steps

--- src/app/rules/ts/User.oss.ts
+++ src/app/rules/ts/User.oss.ts
@@ -5,7 +5,7 @@
  *
  */
 /* tslint:disable */
-export const UserRule = '/*     User rules.oss -- Meta-data rules. Generated file      Default definition */ class=User {     field {       after: zLeft;     }      field=accountType {         trait:asSelect;         choices:["Personal", "Business"];     }      field=contactMethod {         choices:["Email", "Phone", "Mail"];     }      field=martialStatus {         trait:asSelect;         choices:["Single", "Married"];     } }             ';
+export const UserRule = 'class=User {    field {   	placeholder: "Enter a value";   }     field=(firstName, lastName) {     trait: required;    }      field=accountType {         trait:asRadio;         choices:["Personal", "Business"];     }      field=contactMethod {         choices:["Email", "Phone", "Mail"];     }      field=martialStatus {         trait:asSelect;         choices:["Single", "Married"];     }      field=numOfChildren {       visible: ${object.accountType === "Personal"};     }      zNone => *;     zLeft => uniqueName => firstName => lastName => birthdate => martialStatus;     zRight => employer => email => accountType => numOfChildren => contactMethod; }    class=User {    operation=(edit, view) {       zNone => *;       zLeft => firstName => lastName => birthdate => martialStatus;       zRight => employer => email => accountType => numOfChildren => contactMethod;    }    operation=(create) {       zNone => *;       zLeft => firstName => lastName => birthdate => martialStatus;    } }   role=admin class=User {    zNone => *;    zLeft => uniqueName => firstName => lastName => birthdate => martialStatus => employer => email => accountType => numOfChildren => contactMethod; }    ';
 /* tslint:disable */
 /**
  *  @formatter:on
